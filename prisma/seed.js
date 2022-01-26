5245dfe0df81ec7c21e659fc5186598d8cf28a45
Can-Tina <lennihart@gmail.com>
2022-01-26 16:52:27 +0000

    sssss

--- prisma/seed.js
+++ prisma/seed.js
@@ -17,6 +17,20 @@ async function seed() {
 
     console.log('Customer created', createdCustomerAndContact);
 
+    const createdMovieAndScreenings = await prisma.movie.create({
+        data: {
+            title: 'Getting All Achievements in 3 hours - Welcome to the game 2 [World Record]',
+            runtimeMins: 187,
+            Screenings: {
+                create: {
+                    startsAt: '2020-02-29T19:20:30.451Z'
+                }
+            }
+        }
+    })
+
+    console.log('Movie created', createdMovieAndScreenings);
+
     // Add your code here
     /*const createdContact = await prisma.contact.create({
         data: {
@@ -25,16 +39,7 @@ async function seed() {
         }
     })
 
-    console.log('Contact created', createdContact);*/
-
-    const createdMovie = await prisma.movie.create({
-        data: {
-            title: 'Getting All Achievements in 3 hours - Welcome to the game 2 [World Record]',
-            runtimeMins: 187
-        }
-    })
-
-    console.log('Movie created', createdMovie);
+    console.log('Contact created', createdContact);
 
     const createdScreening = await prisma.screening.create({
         data: {
@@ -42,7 +47,7 @@ async function seed() {
         }
     })
 
-    console.log("Screening created", createdScreening)
+    console.log("Screening created", createdScreening)*/
 
     // Don't edit any of the code below this line
     process.exit(0);
