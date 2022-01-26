a85fa7831fa0d97e0db03e6fc87dd06f39dc82e7
Can-Tina <lennihart@gmail.com>
2022-01-26 12:18:21 +0000

    First Task Done (?)

--- prisma/seed.js
+++ prisma/seed.js
@@ -11,9 +11,14 @@ async function seed() {
     console.log('Customer created', createdCustomer);
 
     // Add your code here
+    const createdContact = await prisma.contact.create({
+        data: {
+            phone: '+44 65656 656565',
+            email: 'havethebestgoats@gmail.com'
+        }
+    })
 
-
-
+    console.log('Contact created', createdContact);
 
     // Don't edit any of the code below this line
     process.exit(0);
